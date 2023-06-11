be9e8f654dfbdd1c200ffe3d071a59f3fbf15392
hyebeen byun <hyebeen2658@naver.com>
2023-06-11 22:52:37 +0900

    [a11y] Esc 누르면 동영상 멈춤 기능 적용

--- kiyomi/src/components/Home/HomeBanner.tsx
+++ kiyomi/src/components/Home/HomeBanner.tsx
@@ -1,8 +1,13 @@
+import { useVideoControls } from "@/hooks/useVideoControls   ";
+
 export function HomeBanner() {
+  const { videoRef, toggleVideo } = useVideoControls();
+
   return (
     <div className="w-100 max-h-100 bg-gray-900 rounded-lg overflow-hidden shadow-lg">
       <div className="relative h-0 pb-[56.25%]">
         <video
+          ref={videoRef}
           className="absolute top-0 left-0 w-full h-full object-cover"
           src="/assets/video/ClothVideo.mp4"
           autoPlay
@@ -13,13 +18,15 @@ export function HomeBanner() {
           <div className="flex space-x-4">
             <button
               type="button"
-              className="px-8 py-2 text-black rounded bg-slate-100"
+              className="px-10 py-2 text-black rounded bg-slate-100"
+              onClick={toggleVideo}
             >
               남성
             </button>
             <button
               type="button"
-              className="px-8 py-2 text-black rounded bg-slate-100"
+              className="px-10 py-2 text-black rounded bg-slate-100"
+              onClick={toggleVideo}
             >
               여성
             </button>
