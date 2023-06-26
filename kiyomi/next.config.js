45493d4e2457f3ca64fade01cd7b7bb2def33948
hyebeen byun <hyebeen2658@naver.com>
2023-06-26 23:47:29 +0900

    [setting] @svgr/webpack 설치 후 속성 추가

--- kiyomi/next.config.js
+++ kiyomi/next.config.js
@@ -3,6 +3,20 @@ const nextConfig = {
   images: {
     domains: ["image.thehyundai.com"],
   },
+  rules: [
+    {
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    },
+  ],
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      use: ["@svgr/webpack"],
+    });
+
+    return config;
+  },
 };
 
 module.exports = nextConfig;
