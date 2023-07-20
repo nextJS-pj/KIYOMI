d17375ce4fb38432f6c9ef2dabc963059cdfcfe3
hyebeen byun <hyebeen2658@naver.com>
2023-07-20 14:45:21 +0900

    [feat] react-hook-form으로 회원가입 폼 구현 후 파이어베이스에 연결

--- kiyomi/src/components/RegisterForm/RegisterForm.tsx
+++ kiyomi/src/components/RegisterForm/RegisterForm.tsx
@@ -1,14 +1,17 @@
 "use client";
-import { useForm, SubmitHandler } from "react-hook-form";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
 import classes from "./RegisterForm.module.css";
-import { useRecoilState } from "recoil";
-import { formDataAtom } from "@/@recoil/formData";
+import { useForm, SubmitHandler } from "react-hook-form";
+import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
+import { auth } from "@/firebase/firebase";
 
 interface FormData {
   username: string;
+  email: string;
   password: string;
   confirmPassword: string;
-  phone: number;
+  phone: string;
 }
 
 export function RegisterForm() {
@@ -19,10 +22,38 @@ export function RegisterForm() {
     getValues,
   } = useForm<FormData>();
 
-  // const [formData, setFormData] = useRecoilState(formDataAtom);
+  const router = useRouter();
+  const [isLoading, setIsLoading] = useState(false);
 
-  const onSubmit: SubmitHandler<FormData> = (data) => {
-    console.log(data);
+  const onSubmit: SubmitHandler<FormData> = async (data) => {
+    setIsLoading(true);
+
+    try {
+      console.log("Submitting form data:", data);
+
+      const userCredential = await createUserWithEmailAndPassword(
+        auth,
+        data.email,
+        data.password
+      );
+
+      const user = userCredential.user;
+
+      console.log("User created:", user);
+
+      await updateProfile(user, {
+        displayName: data.username,
+      });
+
+      console.log("User profile updated:", user);
+
+      router.push("/");
+    } catch (error) {
+      console.error("Error creating user:", error);
+      alert("회원가입에 실패했습니다. 나중에 다시 시도해주세요.");
+    } finally {
+      setIsLoading(false);
+    }
   };
 
   return (
@@ -52,6 +83,31 @@ export function RegisterForm() {
             <span className="text-red-500">{errors.username.message}</span>
           )}
         </div>
+
+        <div className="mb-4">
+          <label htmlFor="email" className="text-sm">
+            이메일
+          </label>
+          <input
+            id="email"
+            type="text"
+            placeholder="이메일"
+            {...register("email", {
+              required: "이메일을 입력하세요",
+              pattern: {
+                value: /^[\w.-]+@[a-zA-Z\d.-]+\.[a-zA-Z]{2,}$/,
+                message: "유효한 이메일 주소를 입력하세요",
+              },
+            })}
+            className={`w-full py-2 px-4 border ${
+              errors.email ? "border-red-500" : "border-gray-300"
+            } rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500`}
+          />
+          {errors.email && (
+            <span className="text-red-500">{errors.email.message}</span>
+          )}
+        </div>
+
         <div className="mb-4">
           <label htmlFor="password" className="text-sm">
             비밀번호
@@ -77,6 +133,7 @@ export function RegisterForm() {
             <span className="text-red-500">{errors.password.message}</span>
           )}
         </div>
+
         <div className="mb-4">
           <label htmlFor="confirmPassword" className="text-sm">
             비밀번호 확인
@@ -99,32 +156,9 @@ export function RegisterForm() {
             <p className="text-red-500">{errors.confirmPassword.message}</p>
           )}
         </div>
-        <div className="mb-4">
-          <label htmlFor="phone" className="text-sm">
-            전화번호
-          </label>
-          <input
-            id="phone"
-            type="tel"
-            placeholder="전화번호"
-            {...register("phone", {
-              required: "전화번호를 입력하세요",
-              pattern: {
-                value: /^\d{3}\d{3,4}\d{4}$/,
-                message: "유효한 전화번호를 입력하세요 (예: 01012345678)",
-              },
-            })}
-            className={`w-full py-2 px-4 border ${
-              errors.username ? "border-red-500" : "border-gray-300"
-            } rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500`}
-          />
-          {errors.phone && (
-            <p className="text-red-500">{errors.phone.message}</p>
-          )}
-        </div>
 
-        <button type="submit" className={classes.button}>
-          가입하기
+        <button type="submit" className={classes.button} disabled={isLoading}>
+          {isLoading ? "가입 중..." : "가입하기"}
         </button>
       </form>
     </div>
